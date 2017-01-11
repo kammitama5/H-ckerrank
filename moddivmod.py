a3f8dbddbd9ea543f763af9fb65c1329c71ce60f
kammitama5 <krystal.maughan@gmail.com>
2017-01-11 08:58:39 -0800

    solved

---
+++ moddivmod.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from __future__ import division
+a = int(raw_input())
+b = int(raw_input())
+#integer division //
+print a // b
+#modulo % 
+print a % b
+#divmod => divmod(a, b)
+print divmod(a, b)
