68be2ceb57c3a2cfd0ca25d99c19c8084b1ef189
kammitama5 <krystal.maughan@gmail.com>
2017-03-19 19:46:59 -0700

    equality for all

---
+++ equalityswitchstatements.js
@@ -0,0 +1,19 @@
+// Write your code below!
+var animal = prompt("Pick an animal");
+
+switch(animal){
+    case "dog":
+        console.log("Woof");
+        break;
+    case "cat":
+        console.log("Meow");
+        break;
+    case "feminist":
+        console.log("whine");
+        break;
+    default:
+        console.log("I'm just a moose"); 
+    
+    }
+	
+	
